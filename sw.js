bcfee74b872a65b0f621c23c133baf2027d3a004
olehkostiukcctv-code <olehkostiukcctv@gmail.com>
2026-09-19 21:52:22 +0200

    Update cache name to parle-v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'parle-v1';
+const CACHE_NAME = 'parle-v2';
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(['/', '/index.html', '/manifest.json']))
